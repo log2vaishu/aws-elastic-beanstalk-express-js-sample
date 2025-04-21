3b25f0c1c964e726864814e4b010b33374a7c1be
log2vaishu <76922244+log2vaishu@users.noreply.github.com>
2025-04-20 21:07:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to my Devops Project & successfully setup CI/CD Pipeline'));
+app.get('/', (req, res) => res.send('Welcome to my Devops Project & successfully setup completed of CI/CD Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
